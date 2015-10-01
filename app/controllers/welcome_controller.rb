9589ffc197ca794456d67f335bfe427730ff0d8d
whyayala <whyayala@live.com>
2015-10-01 02:30:52 +0000

    Commiting so i can push(server side) then pull locally (client side)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,11 @@ class WelcomeController < ApplicationController
               	  description: 'Mere Image Salon.',
               	  keywords: 'Site, Login, Members'
   end
+
+  def contact
+        set_meta_tags title: 'Contact Us',
+                  description: 'Mere Image Salon contact page.',
+                  keywords: 'Site, Login, Members'
+  end
+
 end
