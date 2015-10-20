2cb77ecf68cd961ec64b9ae61b4e06ff557d5876
whyayala <whyayala@live.com>
2015-10-20 20:07:18 +0000

    Added Google maps api to Welcome#contact page

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe WelcomeController do
-
-  describe "GET 'contact'" do
-    it "returns http success" do
-      get 'contact'
-      response.should be_success
+  context 'user is not logged in' do
+    describe "GET 'contact'" do
+      it "returns http success" do
+        get 'contact'
+        response.should be_success
+      end
+    end
+    describe "GET 'index'" do
+      it "returns http success" do
+        get 'index'
+        response.should be_success
+      end
     end
   end
-
 end
